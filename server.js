f444f3f93ed564a5ad14cd2bf3388aff667a8d0f
Felipe <videirafelipe020@gmail.com>
2020-05-16 14:46:33 -0300

    redirecting http to https

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const path = require('path');
 
 const app = express();
 
+if(process.env.NODE_ENV === 'production') {
+    app.use((req, res, next) => {
+        if (req.header('x-forwarded-proto') !== 'https')
+            res.redirect(`https://${req.header('host')}${req.url}`)
+        else
+            next()
+    })
+}
+
 app.use(express.static(__dirname + '/dist/pwApk'));
 
 app.get('/.well-known/assetlinks.json', (req, res) => {
